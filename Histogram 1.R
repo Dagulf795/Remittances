2bef68a57773e99e90f9eb7b0084299008e49a83
Dagulf795 <46326330+Dagulf795@users.noreply.github.com>
2022-01-05 15:02:34 +0000

    Update and rename app.R to Histogram 1.R

--- Histogram 1.R
+++ Histogram 1.R
@@ -1,10 +1,5 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+### This is is a Shiny app - this file needs to be renamed app.R, and placed in a folder with the relevant CSVs - for this app, it's pridata.csv and rem_tot.csv. 
 #
 
 library(shiny)
